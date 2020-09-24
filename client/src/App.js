0e7b5fba6a9344135c05859fcaba27dddc3460d4
Stheven Cabral <stheven.cabral@gmail.com>
2020-09-24 14:35:08 -0700

    save project.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './svgs/logo.svg';
+import mangologo from './svgs/mangologo.svg';
 
 function App() {
   return (
     <div className="App">
       <h1>WILD MANGOES</h1>
       <img src={logo} className="App-logo" alt="logo" />
+      <img src={mangologo} className="apparel-logo" alt="apparel-logo" />
     </div>
   );
 }
