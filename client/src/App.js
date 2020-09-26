e8e6999e86f0f8788ec1f9b4c60c5ae422c2d137
Stheven Cabral <stheven.cabral@gmail.com>
2020-09-26 12:04:15 -0700

    Update files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import logo from './svgs/logo.svg';
-import mangologo from './svgs/mangologo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>WILD MANGOES</h1>
-      <img src={logo} className="App-logo" alt="logo" />
-      <img src={mangologo} className="apparel-logo" alt="apparel-logo" />
-    </div>
+    <Router></Router>
   );
 }
 
---
+++ client/src/components/Overlay.js
@@ -0,0 +1,1 @@
+import mangologo from './svgs/mangologo.svg';
