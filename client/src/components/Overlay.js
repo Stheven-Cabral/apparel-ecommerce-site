010c399e39ced6b2c277bad77e8aed86059427d0
Stheven Cabral <stheven.cabral@gmail.com>
2020-09-26 14:34:28 -0700

    Update overlay component.

--- client/src/components/Overlay.js
+++ client/src/components/Overlay.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import mangologo from '../svgs/mangologo.svg';
+import { css } from 'emotion';
+
+const overlayContainer = css`
+  display: flex;
+  justify-content: center;
+`;
+
+const mangoLogoStyle = css`
+  width: 30em;
+  height: auto;
+  margin-top: 5em;
+`
 
 const Overlay = () => {
   return(
-    <div>
-      <img src={mangologo} alt="Wild Mango Logo"/>
+    <div className={overlayContainer}>
+      <img className={mangoLogoStyle} src={mangologo} alt="Wild Mango Logo"/>
     </div>
   )
 }
