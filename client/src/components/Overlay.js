a752083f15d36def52725dc334c492816cb5a542
Stheven Cabral <stheven.cabral@gmail.com>
2020-09-27 13:57:30 -0700

    Update overlay component

--- client/src/components/Overlay.js
+++ client/src/components/Overlay.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import mangologo from '../svgs/mangologo.svg';
+import mangologoOrange from '../svgs/mangologo-orange.svg';
+// import { jsx, css } from '@emotion/core';
 import { css } from 'emotion';
+import 'animate.css';
 
-const overlayContainer = css`
+const logoContainer = css`
   display: flex;
   justify-content: center;
+  position: relative;
 `;
 
 const mangoLogoStyle = css`
-  width: 30em;
+  width: 40rem;
   height: auto;
-  margin-top: 5em;
+  margin-top: 55px;
+  animation: bounceIn;
+  animation-duration: 3s;
+  position: absolute;
+  z-index: 2;
+`
+
+const mangoLogoOrangeStyle = css`
+  width: 40rem;
+  height: auto;
+  margin-top: 55px;
+  animation: bounceIn;
+  animation-duration: 5s;
+  position: absolute;
+  transform: scale(1.02);
+  z-index: 1;
+`
+const apparelName = css`
+  font-size: 6.5rem;
+  font-family: 'Kaushan Script', cursive;
+  position: absolute;
+  top: 600px;
+  left: 480px;
+`
+
+const apparelName2 = css`
+  color: #ffc42e;
 `
 
 const Overlay = () => {
   return(
-    <div className={overlayContainer}>
-      <img className={mangoLogoStyle} src={mangologo} alt="Wild Mango Logo"/>
-    </div>
+    <React.Fragment>
+      <div className={logoContainer}>
+        <img className={mangoLogoStyle} src={mangologo} alt="Wild Mango Logo"/>
+        <img className={mangoLogoOrangeStyle} src={mangologoOrange} alt="Wild Mango Logo"/>
+        <h1 className={apparelName}>Wild<span className={apparelName2}>&nbsp;Mango</span>&nbsp;Apparel</h1>
+      </div>
+    </React.Fragment>
   )
 }
 
