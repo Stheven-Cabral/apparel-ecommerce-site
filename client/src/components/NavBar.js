632732bd1f942224d118bbc75d1d7e81a9228ea4
Stheven Cabral <stheven.cabral@gmail.com>
2020-09-29 20:30:41 -0700

    Update NavBar component.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,19 +2,39 @@
 import { jsx, css } from '@emotion/core'
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import mangologo from '../svgs/mangologo.svg';
+import mangologowhite from '../svgs/mangologo.svg';
 // import 'animate.css';
+
+const logoStyle = css`
+  width: 50px;
+  height: 50px;
+  background-color: #ffc42e;
+`;
+
+const navBarStyle = css`
+  height: 60px;
+  background-color: rgb(24, 24, 24);
+`;
+
+const logoTextStyle = css`
+  text-decoration: none;
+  display: flex;
+  margin-left: 20px;
+  height: 100%;
+`;
+
+const mangoesStyle = css`
+  color: #ffc42e;
+`;
 
 const NavBar = () => {
   return(
     <React.Fragment>
-      <div className="home-header">
-        <nav className="nav-bar">
-          <Link to="/home">
-            <h1>Wild Mangoes</h1>
-          </Link>
-        </nav>
-      </div>
+      <nav className="nav-bar" css={navBarStyle}>
+        <Link to="/home" css={logoTextStyle}>
+          <h1>Wild<img src={mangologowhite} css={logoStyle} alt='Mango Logo'/><span css={mangoesStyle}>&nbsp;Mangoes</span></h1>
+        </Link>
+      </nav>
     </React.Fragment>
   )
 }
