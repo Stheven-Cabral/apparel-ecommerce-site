986030251168eae8d76325f0ad2080c990f22b25
Stheven Cabral <stheven.cabral@gmail.com>
2020-10-03 20:37:00 -0700

    Added the bootstrap library.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,29 @@
-import React from 'react';
+
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';import React from 'react';
 // import { Link } from 'react-router-dom';
 // import mangologo from '../svgs/mangologo.svg';
 // import mangologoOrange from '../svgs/mangologo-orange.svg';
-// import { css } from 'emotion';
 import 'animate.css';
 import NavBar from './NavBar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const containerStyles = css`
+  background-color: blue;
+`;
 
 const Home = () => {
   return(
     <React.Fragment>
       <NavBar />
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Container css={containerStyles} fluid>
+        <Row>
+          <Col><h1>Home</h1></Col>
+          <Col><h1>History</h1></Col>
+        </Row>
+      </Container>
     </React.Fragment>
   )
 }
