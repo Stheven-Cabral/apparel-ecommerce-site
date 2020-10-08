2f36f7965cecf9d727f79b099644acba18448bed
Stheven Cabral <stheven.cabral@gmail.com>
2020-10-07 19:58:12 -0700

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,7 +2,9 @@
 import { jsx, css } from '@emotion/core';
 import Container from 'react-bootstrap/Container';
 import NavBar from 'react-bootstrap/NavBar';
-import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import toggle from '../svgs/toggle.svg'
+// import Nav from 'react-bootstrap/Nav';
 
 const containerStyle = css`
   padding: 0;
@@ -30,7 +32,18 @@ const textStyle = css`
   color: #f8f8f8;
 `;
 
-const navLinkClasses = `text-light`;
+// const navLinkClasses = `text-light`;
+
+const toggleStyle = css`
+  background-color: black;
+  border-color: black;
+  height: 50px;
+  width: 50px;
+`;
+
+const toggleButtonStyle = css`
+  transform: scale(1.5);
+`;
 
 const mangoesStyle = css`
   color: #ffc42e;
@@ -40,8 +53,9 @@ const mangoesStyle = css`
 const NavigationBar = () => {
   return(
     <Container fluid css={containerStyle} className="">
-      <NavBar expand="lg" bg="dark" variant="dark" css={navBarStyle} >
-        <NavBar.Toggle aria-controls="basic-navbar-nav" className="" id="toggle-style"/>
+      <NavBar expand="sm" bg="dark" variant="dark" css={navBarStyle} >
+        <Button css={toggleStyle}><img css={toggleButtonStyle} src={toggle} alt="toggle button"/></Button>
+        {/* <NavBar.Toggle aria-controls="basic-navbar-nav" className="" id="toggle-style"/>
         <NavBar.Collapse>
           <Nav className="text-light">
             <Nav.Item><Nav.Link className={navLinkClasses} href="/home">Home</Nav.Link></Nav.Item>
@@ -50,7 +64,7 @@ const NavigationBar = () => {
             <Nav.Item><Nav.Link className={navLinkClasses} href="/shop">Shop</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link className={navLinkClasses} href="/contact">Contact</Nav.Link></Nav.Item>
           </Nav>
-        </NavBar.Collapse>
+        </NavBar.Collapse> */}
       </NavBar>
       <div css={brandStyle}><h1 css={textStyle}>Wild<span css={mangoesStyle}>&nbsp;Mangoes</span></h1></div>
     </Container>
