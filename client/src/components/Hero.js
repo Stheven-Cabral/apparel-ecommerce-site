494feea72d738de06b3bd9067c291862af826c0b
Stheven Cabral <stheven.cabral@gmail.com>
2020-10-08 20:10:10 -0700

    Update Hero.js image.

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,18 +1,20 @@
 /** @jsx jsx */
 import React from 'react';
 import Mangoes from '../images/mango-hero-bg.png';
-import { jsx, css } from '@emotion/core';
+import MangoesMobile from '../images/mango-hero-bg-mobile.png';
+import { jsx } from '@emotion/core';
+import Image from 'react-bootstrap/Image';
 
-const heroStyle = css`
-  width: 100vw;
-`;
+// const heroStyle = css`
+//   width: 100vw;
+// `;
 
 
 const Hero = () => {
 
   return(
     <React.Fragment>
-      <img src={Mangoes} alt="Collection of Mangoes" css={heroStyle}></img>
+      <Image sizes="(max-width: 500px) 100vw, (max-width: 900px) 50vw, 800px" srcSet={`${MangoesMobile} 500w, ${Mangoes} 800w`} alt="Collection of Mangoes" fluid></Image>
     </React.Fragment>
   )
 }
