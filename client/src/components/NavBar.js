519c41a5f1152116db5f217147294a50416f5d0b
Stheven Cabral <stheven.cabral@gmail.com>
2020-10-08 19:01:09 -0700

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,8 +7,9 @@ import Container from 'react-bootstrap/Container';
 import NavBar from 'react-bootstrap/NavBar';
 import toggle from '../svgs/toggle.svg';
 import shoppingCart from '../svgs/shopping-cart.svg';
-import 'animate.css';
 
+
+/** Navigation Bar Component Styling */
 const containerStyle = css`
   padding: 0;
   position: relative;
@@ -46,12 +47,15 @@ const toggleStyle = css`
   width: 45px;
   transition: all .2s ease;
   &:hover {
-    background-color: #ffc42e;
+    background-color: #f3dd19;
     border: 2px solid #1F1F1F;
     border-radius: 10%;
   }
   &:focus {
     outline: none;
+  }
+  @media(min-width: 992px) {
+    display: none;
   }
 `;
 
@@ -70,34 +74,49 @@ const NavLinksContainer = styled.div`
   top: 60px;
   transform: ${({visibleNav}) => visibleNav ? 'translateX(0%)' : 'translateX(-100%)'};
   transition: transform .3s ease;
+  @media(min-width: 992px) {
+    height: 65px;
+    left: 0px;
+    top: 0px;
+    transform: translateX(0%);
+  }
 `;
 
 const UnorderedList = styled.ul`
   display: ${({visibleNav}) => visibleNav ? 'block' : 'none'};
   list-style: none;
   padding: 0px;
+  @media (min-width: 992px) {
+    background-color: blue;
+  }
 `;
 
 const linkStyle = css`
   font-family: 'Bebas Neue', cursive;
   padding: 2.5px 0px;
   color: white;
+  @media (min-width: 992px) {
+    padding: 0;
+    margin: 0;
+  }
 `;
 
 const mangoesStyle = css`
-  color: #ffc42e;
+  color: #f3dd19;
 `;
 
 const shoppingCartButtonStyle = css`
-  background-color: #1F1F1F;
+  background-color: transparent;
   border: none;
-  height: 50px;
-  width: 50px;
+  height: 55px;
+  width: 55px;
   position: absolute;
   z-index: 4;
+  right: 17px;
+  top: 5px;
 `;
 
-
+/**Navigation Bar Component */
 const NavigationBar = () => {
   const [visibleNav, setNavVisible] = useState(false);
 
@@ -106,11 +125,11 @@ const NavigationBar = () => {
       <NavBar variant="dark" css={navBarStyle} >
         <button css={toggleStyle} onClick={toggleNavVisibility}><img css={toggleButtonStyle} src={toggle} alt="toggle button"/></button>
         <NavLinksContainer visibleNav={visibleNav}>
-          <UnorderedList visibleNav={visibleNav}>
-            <li className="text-center"><Link css={linkStyle}>HOME</Link></li>
-            <li className="text-center"><Link css={linkStyle}>ABOUT</Link></li>
-            <li className="text-center"><Link css={linkStyle}>DESIGNS</Link></li>
-            <li className="text-center"><Link css={linkStyle}>SHOP</Link></li>
+          <UnorderedList visibleNav={visibleNav} className="d-lg-flex">
+            <li className="text-center text-lg-left mx-lg-3"><Link css={linkStyle}>HOME</Link></li>
+            <li className="text-center text-lg-left mx-lg-3"><Link css={linkStyle}>ABOUT</Link></li>
+            <li className="text-center text-lg-left mx-lg-3"><Link css={linkStyle}>DESIGNS</Link></li>
+            <li className="text-center text-lg-left mx-lg-3"><Link css={linkStyle}>SHOP</Link></li>
           </UnorderedList>
         </NavLinksContainer>
       </NavBar>
