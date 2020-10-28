ac13f14c5affae2eca70f14d25929455df3a6e06
Stheven Cabral <stheven.cabral@gmail.com>
2020-10-27 17:19:08 -0700

    Start About component.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,24 +4,29 @@ import React from 'react';
 // import Mangoes from '../images/mango-hero-bg.png';
 // import mangoLogoWhite from '../svgs/mangologo-white.svg';
 // import { jsx } from '@emotion/core';
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
 // import Image from 'react-bootstrap/Image';
 // import ShopButton from './ShopButton';
 // import DesignButton from './DesignButton';
 
-// const AboutContainer = styled.div`
-//   display: flex;
-// `;
+const AboutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 
 const About = () => {
 
   return(
     <React.Fragment>
-      <div>
-        <h1>The Beginning</h1>
+      <AboutContainer>
+        <div className="title-container">
+          <h1>The Beginning</h1>
+        </div>
+        
         <p>Born from a passion for style, art, and urban design, Wild Mangoes was birthed in order to express ones self.</p>
-      </div>
+      </AboutContainer>
   </React.Fragment>
   )
 }
